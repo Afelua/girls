e0a115ab40285a2b30c161ed8d0dd1bfacad13ba
Елена Семенова <elena.semenova_2012@mail.ru>
2018-06-26 21:11:54 +0300

    Lesson 8

---
+++ script.js
@@ -0,0 +1,25 @@
+var weather = 19; // число
+
+var message = "Привет!" // строка
+
+var isShow = true; // boolean/логический
+
+console.log(typeof (weather));
+
+console.log(weather);
+console.log(message);
+
+
+if (weather > 20){
+  console.log('Хорошая погода!');
+}
+
+
+if (weather > 15){
+  alert('Нормальная погода!');
+}
+
+var age = prompt("Сколько вам лет?");
+console.log(age);
+
+confirm('Вам больше 18?');
